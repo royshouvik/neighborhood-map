7ed1a93b11fbe9ede7ff826496ebfeac5e2ee75a
Shouvik Roy <shouvik@techie.com>
2016-05-15 12:21:59 +0530

    Added geolocation

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
-// function getlocation() {
-//     if ("geolocation" in navigator) {
-//         /* geolocation is available */
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             getrestaurents(position.coords.latitude, position.coords.longitude);
-//         });
-//     } else {
-//          geolocation IS NOT available 
-//     }
-// }
+function getlocation() {
+    if ("geolocation" in navigator) {
+        /* geolocation is available */
+        navigator.geolocation.getCurrentPosition(function(position) {
+            return position;
+        });
+    } else {
+         // geolocation IS NOT available 
+    }
+}
 
 
 
-// function dcoumentHeight() {
-//     var windowHeight = $(window).height();
-//     $('#map').height(windowHeight);
-// }
+function dcoumentHeight() {
+    var windowHeight = $(window).height();
+    $('#map').height(windowHeight);
+}
 
-// $(window).resize(dcoumentHeight);
+$(window).resize(dcoumentHeight);
 
 
-// function AppViewModel() {
-//     var self = this;
-//     self.restuarents = ko.observableArray([]);
+function AppViewModel() {
+    var self = this;
+    self.restuarents = ko.observableArray([]);
+    self.position = getlocation();
 
-// }
+}
 
-// ko.applyBindings(new AppViewModel());
+ko.applyBindings(new AppViewModel());
 
-$.ajax({url: "https://developers.zomato.com/api/v2.1/search",
-    type: "GET",
-    crossDomain: true,
-    headers: { 'user-key': '38c42097628d298c85e496cd81990b3a' },
 
-        
-      });
+
+
+
+
+// $.ajax({
+//   type: 'GET',
+//   url: "https://developers.zomato.com/api/v2.1/search?count=10",
+//   contentType: 'application/json',
+//   xhrFields: {
+//     withCredentials: true
+//   },
+//   headers: {
+//     'X-Zomato-API-Key' : '38c42097628d298c85e496cd81990b3a'
+//   },
+//   success: function() {
+//     // Here's where you handle a successful response.
+//     alert('success');
+//   },
+//   error: function() {
+//   }
+// });
