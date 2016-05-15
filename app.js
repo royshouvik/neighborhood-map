337e3e8b3be206cfb43ed99f07bf109caf5f42a5
Shouvik Roy <shouvik@techie.com>
2016-05-15 13:10:15 +0530

    Added zomato api

--- app.js
+++ app.js
@@ -1,81 +1,86 @@
-    var view = {
-        getLocation: function() {
-        	var self = this;
-            if ("geolocation" in navigator) {
-                /* geolocation is available */
-                view.position = navigator.geolocation.getCurrentPosition(function(position) {
-                    console.log(position);
-                    self.initMap(position.coords.latitude, position.coords.longitude);
-                });
-            } else {
-                // geolocation IS NOT available 
-            }
-        },
-
-        init: function() {
-            this.getLocation();
-        },
-
-        initMap: function(latitude, longitude) {
-            var windowHeight = $(window).height();
-            $('#map').height(windowHeight);
-
-            var mapDiv = document.getElementById('map');
-            var map = new google.maps.Map(mapDiv, {
-                center: {
-                    lat: latitude,
-                    lng: longitude
-                },
-                zoom: 15,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                disableDefaultUI: true
+var view = {
+    getLocation: function() {
+        var self = this;
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+            view.position = navigator.geolocation.getCurrentPosition(function(position) {
+                console.log(position);
+                self.position = position;
+                self.initMap(position.coords.latitude, position.coords.longitude);
             });
+        } else {
+            // geolocation IS NOT available 
         }
-    };
+    },
 
-    view.init();
+    init: function() {
+        this.getLocation();
+        this.updateHeight();
+    },
+
+    initMap: function(latitude, longitude) {
+        var windowHeight = $(window).height();
+        $('#map').height(windowHeight);
+
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {
+                lat: latitude,
+                lng: longitude
+            },
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true
+        });
+    },
+
+    updateHeight: function() {
+        var windowHeight = $(window).height();
+        $('#map').height(windowHeight);
+    }
+
+};
+
+view.init();
+$(window).resize(view.updateHeight);
+
+
+function eatery(data) {
+	this.name = data.name;
+}
+
+function AppViewModel() {
+    var self = this;
+    self.restuarents = ko.observableArray([]);
+
+
+}
+
+ko.applyBindings(new AppViewModel());
 
 
 
 
 
 
-// function dcoumentHeight() {
-//     var windowHeight = $(window).height();
-//     $('#map').height(windowHeight);
-// }
-
-$(window).resize(dcoumentHeight);
-
-
-// function AppViewModel() {
-//     var self = this;
-//     self.restuarents = ko.observableArray([]);
-//     self.position = getlocation();
-
-// }
-
-// ko.applyBindings(new AppViewModel());
-
-
-
-
-
-
-// $.ajax({
-//   type: 'GET',
-//   url: "https://developers.zomato.com/api/v2.1/search?count=10",
-//   contentType: 'application/json',
-//   xhrFields: {
-//     withCredentials: true
-//   },
-//   headers: {
-//     'X-Zomato-API-Key' : '38c42097628d298c85e496cd81990b3a'
-//   },
-//   success: function() {
-//     // Here's where you handle a successful response.
-//     alert('success');
-//   },
-//   error: function() {
-//   }
-// });
+$.ajax({
+  type: 'GET',
+  url: "https://developers.zomato.com/api/v2.1/search?count=10",
+  contentType: 'application/json',
+  xhrFields: {
+    withCredentials: true
+  },
+  data : {
+  	lat: view.position.coords.latitude,
+  	lon: view.position.coords.longitude
+  },
+  headers: {
+    'X-Zomato-API-Key' : '38c42097628d298c85e496cd81990b3a'
+  },
+  success: function(data) {
+    // Here's where you handle a successful response.
+    console.log(data);
+  },
+  error: function() {
+  }
+});
