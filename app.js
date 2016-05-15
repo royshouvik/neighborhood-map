a06cd430415c9586bed55658f53107fa56fe65e7
Shouvik Roy <shouvik@techie.com>
2016-05-15 11:28:06 +0530

    trying ajax

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
- function dcoumentHeight() {
-     var windowHeight = $(window).height();
-     $('#map').height(windowHeight);
- }
+// function getlocation() {
+//     if ("geolocation" in navigator) {
+//         /* geolocation is available */
+//         navigator.geolocation.getCurrentPosition(function(position) {
+//             getrestaurents(position.coords.latitude, position.coords.longitude);
+//         });
+//     } else {
+//          geolocation IS NOT available 
+//     }
+// }
 
 
 
- $(window).resize(dcoumentHeight);
+// function dcoumentHeight() {
+//     var windowHeight = $(window).height();
+//     $('#map').height(windowHeight);
+// }
+
+// $(window).resize(dcoumentHeight);
+
+
+// function AppViewModel() {
+//     var self = this;
+//     self.restuarents = ko.observableArray([]);
+
+// }
+
+// ko.applyBindings(new AppViewModel());
+
+$.ajax({url: "https://developers.zomato.com/api/v2.1/search?count=20",
+    type: "GET",
+    crossDomain: true,
+    headers: { 'user-key': '38c42097628d298c85e496cd81990b3a' },
+
+        
+      });
